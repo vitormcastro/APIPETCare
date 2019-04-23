989662a973f39259ea392f11dab5c9508a84ec61
FTT-EC <vitormcastro@outlook.com>
2019-04-22 21:55:46 -0300

    Alteração de nome de variavel

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
-var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+var api = express();
+api.use(bodyParser.urlencoded({ extended: false }));
+api.use(bodyParser.json());
 
 //app.use('/api/login', require('./controller/api/login.controller'));
 
 
 var apiPort = process.env.PORT || 6000;
 
-var serverAPI = app.listen(apiPort, function() {
+var serverAPI = api.listen(apiPort, function() {
   console.log('Server API listening at http://' + serverAPI.address().address + ':' + serverAPI.address().port);
 });
