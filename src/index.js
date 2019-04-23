4556bdffc5fc994a41b8cc6ba81e7b7d1df3acfe
FTT-EC <vitormcastro@outlook.com>
2019-04-22 21:52:15 -0300

    Limpeza do codigo

--- src/index.js
+++ src/index.js
@@ -17,26 +17,3 @@ var apiPort = process.env.PORT || 6000;
 var serverAPI = app.listen(apiPort, function() {
   console.log('Server API listening at http://' + serverAPI.address().address + ':' + serverAPI.address().port);
 });
-
-app.get('/maps', function(req, res) {
-    var dados = [
-      {
-        lat: -25.470991, 
-        lon: -49.271036
-      },
-      {
-        lat: -0.935586,
-        lon: -49.635540
-      },
-      {
-        lat: -2.485874, 
-        lon: -43.128493
-      }
-    ];
-  
-    res.send(JSON.stringify(dados));
-  });
-
-  app.listen(8000, function() {
-    console.log('Servidor rodando na porta 8000.');
-  });
