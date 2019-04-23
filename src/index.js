ee660b56f2e3651ecf9398752f1e28f23c9c8ca2
FTT-EC <vitormcastro@outlook.com>
2019-04-22 22:30:40 -0300

    atualização do index e do config

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ require('rootpath')();
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var expressJwt = require('express-jwt');
+var config = require("config.json");
 
 var api = express();
 api.use(bodyParser.urlencoded({ extended: false }));
 api.use(bodyParser.json());
+api.use('/api', expressJwt({ secret: process.env.secret || config.secret }).unless({ path: ['/api/about','/api/users/authenticate', '/api/users/register'] }));
+
 
 //app.use('/api/login', require('./controller/api/login.controller'));
 
 
-var apiPort = process.env.PORT || 6000;
+var apiPort = process.env.PORT || config.port;
 
 var serverAPI = api.listen(apiPort, function() {
   console.log('Server API listening at http://' + serverAPI.address().address + ':' + serverAPI.address().port);
